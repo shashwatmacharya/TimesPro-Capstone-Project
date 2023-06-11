2ebb764a9299db68115f47e44280fff70f28f393
shashwatmacharya <120778674+shashwatmacharya@users.noreply.github.com>
2023-06-11 12:46:18 +0530

    Add files via upload

---
+++ client/src/pages/IndexPage.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import Post from '../components/Post'
+import { useEffect, useState } from "react";
+
+
+function IndexPage() {
+
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:4000/post').then(response => {
+      response.json().then(posts => {
+        setPosts(posts);
+      });
+    });
+  }, []);
+
+  return (
+    <>
+      {posts.length > 0 && posts.map(post => (
+        <Post {...post} />
+      ))}
+    </>
+  )
+}
+
+export default IndexPage
---
+++ client/src/pages/PostPage.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import { useContext, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { formatISO9075 } from "date-fns";
+import { UserContext } from "../UserContext";
+import { Link } from 'react-router-dom';
+
+
+function PostPage() {
+
+    const [postInfo, setPostInfo] = useState(null);
+    const { userInfo } = useContext(UserContext);
+
+    const { id } = useParams();
+    useEffect(() => {
+        fetch(`http://localhost:4000/post/${id}`)
+            .then(response => {
+                response.json().then(postInfo => {
+                    setPostInfo(postInfo);
+                });
+            });
+    }, []);
+
+    if (!postInfo) return '';
+
+    return (
+    <div className='post-page'>
+        <div className='image'>
+            <img src={`http://localhost:4000/${postInfo.cover}`} alt=""/>
+        </div>
+        <h1>{postInfo.title}</h1>
+        <time>{formatISO9075(new Date(postInfo.createdAt))}</time>
+        <div className="author">by @{postInfo.author.username}</div>
+        {userInfo.id === postInfo.author._id && (
+        <div className="edit-row">
+          <Link className="edit-btn" to={`/edit/${postInfo._id}`}>
+            Edit this post
+          </Link>
+        </div>
+      )}
+        <div className="content" dangerouslySetInnerHTML={{__html:postInfo.content}} />
+    </div>
+    );
+}
+
+export default PostPage
